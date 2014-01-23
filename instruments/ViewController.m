9923767276b1b2542b5ecf5b7615ea4eb8024c0f
Taichi Yoshimura <kwsk_greenshad0w6867@yahoo.co.jp>
2014-01-23 16:00:59 +0900

    saisei zissoutyu

--- instruments/ViewController.m
+++ instruments/ViewController.m
@@ -12,7 +12,7 @@
 @interface ViewController (){
     AVAudioRecorder *avRecorder;
    AVAudioPlayer *avPlayer;
-    double s[100];
+    //double s[100];
     int key[100];
 
 @public AVAudioPlayer *audio;
@@ -21,11 +21,17 @@
     int reco;
     int i;
     int j;
+    
+    NSDate *narasitatoki;
+    NSMutableArray *kankakuArray;
 }
 
 @end
 
 @implementation ViewController
+
+- (void)tien{
+}
 
 -(IBAction)btn:(id)sender
 {
@@ -44,15 +50,24 @@
         [audio prepareToPlay];
         
         [audio play];
+        if(reco==1){
         
-        //if(reco==1){
-         //   NSDate *startDate = [NSDate date];
-            
-         //   NSTimeInterval interval = [[NSDate date] timeIntervalSinceDate:startDate];
-         //   NSLog(@"time is %lf (sec)", interval);
-         //   key[j]=1;
-         //   s[j]= interval;
-            
+        if (narasitatoki == nil) {
+            narasitatoki = [NSDate date];
+            kankakuArray = [NSMutableArray new];
+        }else{
+            NSTimeInterval kankaku = [[NSDate date] timeIntervalSinceDate:narasitatoki];
+            [kankakuArray addObject:[NSNumber numberWithDouble:kankaku]];
+            narasitatoki = [NSDate date];
+            key[j]=1;
+            j++;
+            NSLog(@"KANKAKU: %@",kankakuArray);
+        }
+        }
+//        if(reco==1){
+//            key[j]=1;
+//            s[j]= interval;
+        
         
         
     }else if (oto==2){
@@ -67,6 +82,18 @@
         [audio prepareToPlay];
         
         [audio play];
+        if(reco==1){
+        if (narasitatoki == nil) {
+            narasitatoki = [NSDate date];
+            kankakuArray = [NSMutableArray new];
+        }else{
+            NSTimeInterval kankaku = [[NSDate date] timeIntervalSinceDate:narasitatoki];
+            [kankakuArray addObject:[NSNumber numberWithDouble:kankaku]];
+            narasitatoki = [NSDate date];
+            key[j]=2;
+            NSLog(@"KANKAKU: %@",kankakuArray);
+        }
+        }
     }else if (oto==3){
         NSString *path = [[NSBundle mainBundle] pathForResource:@"mi" ofType:@"wav"];
         
@@ -79,6 +106,19 @@
         [audio prepareToPlay];
         
         [audio play];
+        if(reco==1){
+        if (narasitatoki == nil) {
+            narasitatoki = [NSDate date];
+            kankakuArray = [NSMutableArray new];
+        }else{
+            NSTimeInterval kankaku = [[NSDate date] timeIntervalSinceDate:narasitatoki];
+            [kankakuArray addObject:[NSNumber numberWithDouble:kankaku]];
+            narasitatoki = [NSDate date];
+            key[j]=3;
+            j++;
+            NSLog(@"KANKAKU: %@",kankakuArray);
+        }
+        }
     }else if(oto==4){
         NSString *path = [[NSBundle mainBundle] pathForResource:@"fa" ofType:@"wav"];
         
@@ -91,6 +131,19 @@
         [audio prepareToPlay];
         
         [audio play];
+        if(reco==1){
+        if (narasitatoki == nil) {
+            narasitatoki = [NSDate date];
+            kankakuArray = [NSMutableArray new];
+        }else{
+            NSTimeInterval kankaku = [[NSDate date] timeIntervalSinceDate:narasitatoki];
+            [kankakuArray addObject:[NSNumber numberWithDouble:kankaku]];
+            narasitatoki = [NSDate date];
+            key[j]=4;
+            j++;
+            NSLog(@"KANKAKU: %@",kankakuArray);
+        }
+        }
     }else if(oto==5){
         NSString *path = [[NSBundle mainBundle] pathForResource:@"so" ofType:@"wav"];
         
@@ -103,6 +156,19 @@
         [audio prepareToPlay];
         
         [audio play];
+        if(reco==1){
+        if (narasitatoki == nil) {
+            narasitatoki = [NSDate date];
+            kankakuArray = [NSMutableArray new];
+        }else{
+            NSTimeInterval kankaku = [[NSDate date] timeIntervalSinceDate:narasitatoki];
+            [kankakuArray addObject:[NSNumber numberWithDouble:kankaku]];
+            narasitatoki = [NSDate date];
+            key[j]=5;
+            j++;
+            NSLog(@"KANKAKU: %@",kankakuArray);
+        }
+        }
     }else if (oto==6){
         NSString *path = [[NSBundle mainBundle] pathForResource:@"ra" ofType:@"wav"];
         
@@ -115,6 +181,19 @@
         [audio prepareToPlay];
         
         [audio play];
+        if(reco==1){
+        if (narasitatoki == nil) {
+            narasitatoki = [NSDate date];
+            kankakuArray = [NSMutableArray new];
+        }else{
+            NSTimeInterval kankaku = [[NSDate date] timeIntervalSinceDate:narasitatoki];
+            [kankakuArray addObject:[NSNumber numberWithDouble:kankaku]];
+            narasitatoki = [NSDate date];
+            key[j]=6;
+            j++;
+            NSLog(@"KANKAKU: %@",kankakuArray);
+        }
+        }
     }else if (oto==7){
         NSString *path = [[NSBundle mainBundle] pathForResource:@"si" ofType:@"wav"];
         
@@ -127,6 +206,19 @@
         [audio prepareToPlay];
         
         [audio play];
+        if(reco==1){
+        if (narasitatoki == nil) {
+            narasitatoki = [NSDate date];
+            kankakuArray = [NSMutableArray new];
+        }else{
+            NSTimeInterval kankaku = [[NSDate date] timeIntervalSinceDate:narasitatoki];
+            [kankakuArray addObject:[NSNumber numberWithDouble:kankaku]];
+            narasitatoki = [NSDate date];
+            key[j]=7;
+            j++;
+            NSLog(@"KANKAKU: %@",kankakuArray);
+        }
+        }
     }else if (oto==8){
         NSString *path = [[NSBundle mainBundle] pathForResource:@"do2" ofType:@"wav"];
         
@@ -139,7 +231,21 @@
         [audio prepareToPlay];
         
         [audio play];
+        if(reco==1){
+        if (narasitatoki == nil) {
+            narasitatoki = [NSDate date];
+            kankakuArray = [NSMutableArray new];
+        }else{
+            NSTimeInterval kankaku = [[NSDate date] timeIntervalSinceDate:narasitatoki];
+            [kankakuArray addObject:[NSNumber numberWithDouble:kankaku]];
+            narasitatoki = [NSDate date];
+            key[j]=8;
+            j++;
+            NSLog(@"KANKAKU: %@",kankakuArray);
+        }
+        }
     }
+        
     
     
     oto = 0;
@@ -152,26 +258,64 @@
     
     reco = 1;
     
-    NSDate *startDate = [NSDate date];
+   // NSDate *startDate = [NSDate date];
     //do something
-    
-    NSTimeInterval interval = [[NSDate date] timeIntervalSinceDate:startDate];
-    NSLog(@"time is %lf (sec)", interval);
+   
+    //NSTimeInterval interval = [[NSDate date] timeIntervalSinceDate:startDate];
+   // NSLog(@"time is %lf (sec)", interval);
 }
 
 
 - (IBAction)stopbtn:(id)sender {
     
-   
+    reco=0;
     
 }
 
 - (IBAction)playbtn:(id)sender {
+    for (i=0; i<100; i++) {
     
-
+    if(key[i]==1){
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"do" ofType:@"wav"];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    audio = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    }else if(key[i]==2){
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"re" ofType:@"wav"];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    audio = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    }else if(key[i]==3){
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"mi" ofType:@"wav"];
+        NSURL *url = [NSURL fileURLWithPath:path];
+        audio = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    }else if(key[i]==4){
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"fa" ofType:@"wav"];
+        NSURL *url = [NSURL fileURLWithPath:path];
+        audio = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    }else if(key[i]==5){
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"so" ofType:@"wav"];
+        NSURL *url = [NSURL fileURLWithPath:path];
+        audio = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    }else if(key[i]==6){
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"ra" ofType:@"wav"];
+        NSURL *url = [NSURL fileURLWithPath:path];
+        audio = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    }else if(key[i]==7){
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"si" ofType:@"wav"];
+        NSURL *url = [NSURL fileURLWithPath:path];
+        audio = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    }else if(key[i]==8){
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"do2" ofType:@"wav"];
+        NSURL *url = [NSURL fileURLWithPath:path];
+        audio = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    }
+    audio.volume = 0.6;
     
- 
-}   
+    [audio prepareToPlay];
+    
+    [audio play];
+   }
+    [self performSelector:@selector(tien:) withObject:nil afterDelay:[kankakuArray[i] doubleValue]];
+}
 
 
 
@@ -183,6 +327,8 @@
     reco = 0;
     i = 0;
     j = 0;
+    
+    narasitatoki = nil;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
